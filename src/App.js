0e6deafa03af8351662fc05f36def108497709e7
Leonardo <hasorigami@gmail.com>
2023-07-01 17:31:50 -0400

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import StoreNavbar from "./components/NavbarComponent"
+import StoreNavbar from "./components/NavbarComponent";
 import PageHeader from "./components/HeaderComponent";
+import HomePage from "./components/HomePage";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <PageHeader></PageHeader>
       <StoreNavbar />
+      <HomePage />
       <p>Hello world!</p>
     </div>
   );
